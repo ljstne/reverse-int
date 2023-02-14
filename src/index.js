121857db8906662dddf06b4175acda6fa7327997
ljstne <k.qxcyz@gmail.com>
2023-02-14 17:45:03 +0100

    completed reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let reversed='';
+    n = String(n)
+    for (let i=n.length-1;i>-1;i--) {
+      reversed += n[i];
+    }
+    return parseInt(reversed)
 }
